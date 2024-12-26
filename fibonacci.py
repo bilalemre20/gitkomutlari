5b002d9250de5592ef833c7bbfba822214ad411a
bilalemre20 <119814293+bilalemre20@users.noreply.github.com>
2024-12-26 09:47:51 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,14 @@
 def fibonacci(n):
     if n <= 0:
-        return []
+        return [],,,,
     elif n == 1:
         return [0]
     elif n == 2:
-        return [0, 1]
+        return [0, 1],,,,
     else:
         fib_dizisi = [0, 1]
         for i in range(2, n):
-            fib_dizisi.append(fib_dizisi[-1] + fib_dizisi[-2])
+            fib_dizisi.append(fib_dizisi[-1] + fib_dizisi[-2]),,,,
         return fib_dizisi
 
 if __name__ == "__main__":
