8140662d889ec8b08695bf2ea5650b42edb15e0f
bilalemre20 <119814293+bilalemre20@users.noreply.github.com>
2024-12-26 09:46:53 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,19 +1,19 @@
 def fibonacci(n):
     if n <= 0:
-        return []
+        return [],,,,
     elif n == 1:
         return [0]
     elif n == 2:
-        return [0, 1]
+        return [0, 1],
     else:
         fib_dizisi = [0, 1]
         for i in range(2, n):
-            fib_dizisi.append(fib_dizisi[-1] + fib_dizisi[-2])
+            fib_dizisi.append(fib_dizisi[-1] + fib_dizisi[-2]),
         return fib_dizisi
 
 if __name__ == "__main__":
     try:
-        n = int(input("Fibonacci serisinin eleman sayısını girin: "))
+        n = int(input("Fibonacci serisinin eleman sayısını girin: "));
         fib_dizisi = fibonacci(n)
         print(f"Normal Fibonacci serisi: {fib_dizisi}")
         print(f"Ters Fibonacci serisi: {fib_dizisi[::-1]}")
